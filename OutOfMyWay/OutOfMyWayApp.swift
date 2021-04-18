350478392a64fd812fd03b948125e38765cd9e2b
Aiden <meaneea@gmail.com>
2021-04-17 19:36:40 -0500

    Initial Commit

---
+++ OutOfMyWay/OutOfMyWayApp.swift
@@ -0,0 +1,17 @@
+//
+//  OutOfMyWayApp.swift
+//  OutOfMyWay
+//
+//  Created by Tiger Coder on 4/17/21.
+//
+
+import SwiftUI
+
+@main
+struct OutOfMyWayApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
